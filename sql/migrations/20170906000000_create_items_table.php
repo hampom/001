d8934bcb206069b99c0056a5bd3094e754832bc1
anazawa yasuhiro <yasuhiro@humming-code.com>
2017-09-06 16:06:37 +0900

    001.0002

---
+++ sql/migrations/20170906000000_create_items_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class CreateItemsTable extends AbstractMigration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $table = $this->table('items');
+        $table->addColumn('user_id', 'integer', ['signed' => false ])
+              ->addColumn('title', 'string')
+              ->addColumn('desc', 'text')
+              ->addColumn('date', 'date')
+              ->addColumn('done', 'boolean', ['default' => false])
+              ->save();
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        $this->dropTable('items');
+    }
+}
